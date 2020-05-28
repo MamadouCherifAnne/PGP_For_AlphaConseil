737451d5c7eb3db20484dcabe8af6bf4cb48110b
Oumar Ba <bows4844@gmail.com>
2020-05-28 21:12:55 +0200

    un commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+const material = [
+  MatButtonModule,
+  MatToolbarModule,
+];
+@NgModule({
+  imports: [
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+export class MaterialModule { }
